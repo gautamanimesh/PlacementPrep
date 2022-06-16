e8082487b2ba686cb42ac4fceb70b06466e98c9b
N Gautam Animesh <gautamanimesh3@gmail.com>
2022-06-16 16:03:01 +0530

    BinarySearch Code

---
+++ src/placement/prep/BinarySearch.java
@@ -0,0 +1,28 @@
+package placement.prep;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int l, int r, int target) {
+        if (r >= l) {
+            int mid = l + (r-l)/2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] > target) {
+                return binarySearch(arr,l,mid-1,target);
+            }
+            return binarySearch(arr,mid+1,r,target);
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,8,9};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int target = scanner.nextInt();
+        int ans = binarySearch(nums, 0, nums.length-1, target);
+        System.out.println(ans);
+    }
+}
