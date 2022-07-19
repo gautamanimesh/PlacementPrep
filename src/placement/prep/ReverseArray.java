f195a6532354ac95d2c2d242fa9396aa8eb01c0d
N Gautam Animesh <gautamanimesh3@gmail.com>
2022-07-19 11:49:01 +0530

    Codes

--- src/placement/prep/ReverseArray.java
+++ src/placement/prep/ReverseArray.java
@@ -21,7 +21,7 @@ public class ReverseArray {
     }
 
     public static void main(String[] args) {
-        int[] t = {1,2,3,4,5,6,7,8,9};
+        int[] t = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         reverseArray(t);
     }
 }
