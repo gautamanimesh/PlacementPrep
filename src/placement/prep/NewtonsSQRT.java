21db4fcc25eda517d8b945f49ba59868a7650cef
N Gautam Animesh <gautamanimesh3@gmail.com>
2022-07-20 12:58:32 +0530

    Codes

---
+++ src/placement/prep/NewtonsSQRT.java
@@ -0,0 +1,26 @@
+package placement.prep;
+
+public class NewtonsSQRT {
+
+    private static double sqrt (int n) {
+        double root;
+        double x = n;
+
+        while (true) {
+            root = 0.5 * (x + (n/x));
+
+            if (Math.abs(root - x) < 0.01) {
+                break;
+            }
+
+            x = root;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int num = 40;
+        System.out.println(sqrt(num));
+    }
+}
