155a0e8898a35fd97f656d7d6f3842605649728b
N Gautam Animesh <gautamanimesh3@gmail.com>
2022-07-27 23:56:42 +0530

    Codes

--- src/placement/prep/ValidParenthesis.java
+++ src/placement/prep/ValidParenthesis.java
@@ -5,7 +5,9 @@ import java.util.Stack;
 public class ValidParenthesis {
 
     public static boolean isValid (String str) {
+
         Stack<Character> s = new Stack<>();
+
         for (int i=0; i<str.length(); i++) {
             char ch = str.charAt(i);
             if (ch == '{' || ch == '[' || ch == '(') {
