91bad2316526412f34407b9d4e1a166b2497bd82
N Gautam Animesh <gautamanimesh3@gmail.com>
2022-07-27 12:59:30 +0530

    Codes

--- src/placement/prep/MedianOfTwoSortedArrays.java
+++ src/placement/prep/MedianOfTwoSortedArrays.java
@@ -2,6 +2,34 @@ package placement.prep;
 
 public class MedianOfTwoSortedArrays {
 
+    // Time Complexity: O(log(n))
+    private static int medianOptimized (int[] a, int[] b, int startA, int startB, int endA, int endB) {
+        if (endA - startA == 1) {
+            return (Math.max(a[startA], b[startB] + Math.min(a[endA], b[endB])) / 2);
+        }
+
+        int m1 = calculateMedian(a, startA, endA);
+        int m2 = calculateMedian(b, startB, endB);
+
+        if (m1 == m2) {
+            return m1; // return m2;
+        } else if (m1 < m2) {
+            return medianOptimized(a, b, (endA + startA + 1) / 2, startB, endA, (endB + startB + 1) / 2);
+        } else {
+            return medianOptimized(a, b, startA, (endB + startB + 1) / 2, (endA + startA + 1) / 2, endB);
+        }
+    }
+
+    private static int calculateMedian (int[] a, int start, int end) {
+        int n = end - start + 1;
+        if (n % 2 == 0) {
+            return (a[start + (n / 2)] + a[start + (n / 2 - 1)]) / 2;
+        } else {
+            return a[start + n / 2];
+        }
+    }
+
+    // Time Complexity: O(n)
     private static int median (int[] arr1, int[] arr2, int n) {
         int i = 0, j = 0;
         int count;
@@ -34,6 +62,7 @@ public class MedianOfTwoSortedArrays {
         int n2 = arr2.length;
         if (n1 == n2) {
             System.out.println("Median is: " + median(arr1, arr2, n1));
+            System.out.println("Optimized: " + medianOptimized(arr1, arr2, 0, 0, n1-1, n2-1));
         } else {
             System.out.println("Unequal Size");
         }
