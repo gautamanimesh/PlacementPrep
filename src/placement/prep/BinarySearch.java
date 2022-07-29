7bd0830f9ec307c2521393f729390c9540a2311a
N Gautam Animesh <gautamanimesh3@gmail.com>
2022-07-29 15:24:53 +0530

    Codes

--- src/placement/prep/BinarySearch.java
+++ src/placement/prep/BinarySearch.java
@@ -3,16 +3,16 @@ package placement.prep;
 import java.util.Scanner;
 
 public class BinarySearch {
-    public static int binarySearch(int[] arr, int l, int r, int target) {
-        if (r >= l) {
-            int mid = (l+r)/2;
+    public static int binarySearch(int[] arr, int low, int high, int target) {
+        if (high >= low) {
+            int mid = low + (high - low) / 2;
             if (arr[mid] == target) {
                 return mid;
             }
             if (arr[mid] > target) {
-                return binarySearch(arr,l,mid-1,target);
+                return binarySearch(arr, low, mid-1, target);
             }
-            return binarySearch(arr,mid+1,r,target);
+            return binarySearch(arr, mid+1, high, target);
         }
         return -1;
     }
