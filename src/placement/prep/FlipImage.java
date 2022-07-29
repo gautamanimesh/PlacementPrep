a0a3c7f605f84acb272e202b1fc012a2dd03774f
N Gautam Animesh <gautamanimesh3@gmail.com>
2022-07-29 15:28:28 +0530

    Codes

--- src/placement/prep/FlipImage.java
+++ src/placement/prep/FlipImage.java
@@ -3,13 +3,14 @@ package placement.prep;
 import java.util.Arrays;
 
 public class FlipImage {
+
     // Time Complexity: O(N)
     // Space Complexity: O(1)
 
     private static int[][] flipImage(int[][] image) {
         int col = image[0].length;
         for (int[] row: image) {
-            for (int i = 0; i < (col + 1)/2; i++) {
+            for (int i = 0; i < (col + 1) / 2; i++) {
                 // swap
                 int temp = row[i] ^ 1;
                 row[i] = row[col - i - 1] ^ 1;
