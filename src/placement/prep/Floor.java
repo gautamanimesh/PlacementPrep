e5a14c70cbcfec2a8286cf8cde5c69b5406838ab
N Gautam Animesh <gautamanimesh3@gmail.com>
2022-07-29 15:31:34 +0530

    Codes

--- src/placement/prep/Floor.java
+++ src/placement/prep/Floor.java
@@ -3,23 +3,23 @@ package placement.prep;
 public class Floor {
 
     // Floor: Greatest number that is smaller than or equal to target!
-    public static int floor (int[] arr, int l, int r, int target) {
+    public static int floor (int[] arr, int low, int high, int target) {
         if (target < arr[0]) return -1;
-        if (r >= l) {
-            int mid = (l+r)/2;
+        if (high >= low) {
+            int mid = low + (high - low) /2;
             if (arr[mid] == target) {
                 return arr[mid];
             }
             if (arr[mid] > target) {
-                return floor(arr,l,mid-1,target);
+                return floor(arr, low, mid-1, target);
             }
-            return floor(arr,mid+1,r,target);
+            return floor(arr, mid+1, high, target);
         }
-        return arr[r];
+        return arr[high];
     }
 
     public static void main(String[] args) {
-        int[] arr = {2,3,5,9,14,16,18};
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
         int target = 15;
         System.out.println(floor(arr,0,arr.length-1,target));
     }
