1b9f6e9ec56cfee1526d2fc2d703dabcdf9f5e22
N Gautam Animesh <gautamanimesh3@gmail.com>
2022-07-29 15:33:37 +0530

    Codes

--- src/placement/prep/InfiniteArray.java
+++ src/placement/prep/InfiniteArray.java
@@ -7,29 +7,29 @@ public class InfiniteArray {
         int end = 1;
 
         while (target > arr[end]) {
-            int temp = end+1;
+            int temp = end + 1;
             end = end + (end - start + 1);
             start = temp;
         }
         return binarySearch(arr,start,end,target);
     }
 
-    public static int binarySearch(int[] arr, int l, int r, int target) {
-        if (r >= l) {
-            int mid = (l+r)/2;
+    public static int binarySearch(int[] arr, int low, int high, int target) {
+        if (high >= low) {
+            int mid = low + (high - low) /2;
             if (arr[mid] == target) {
                 return mid;
             }
             if (arr[mid] > target) {
-                return binarySearch(arr,l,mid-1,target);
+                return binarySearch(arr, low, mid-1, target);
             }
-            return binarySearch(arr,mid+1,r,target);
+            return binarySearch(arr, mid+1, high, target);
         }
         return -1;
     }
 
     public static void main(String[] args) {
-        int[] arr = {3,5,7,9,10,90,100,130,140,160,170};
+        int[] arr = {3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170};
         int target = 10;
         System.out.println(ans(arr,target));
     }
